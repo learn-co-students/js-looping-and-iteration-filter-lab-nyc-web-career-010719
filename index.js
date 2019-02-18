d784452386900bd3705d7b8bd0835a1db70edcff
jbharwood <josephblakeharwood@gmail.com>
2019-02-17 23:03:01 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solution in this file
+function findMatching(list, name) {
+  return list.filter(function (driverName) {
+    return driverName.toLowerCase() === name.toLowerCase();
+  });
+}
+
+function fuzzyMatch(list, name) {
+  return list.filter(function (driverName) {
+    return driverName[0] === name[0];
+  });
+}
+
+function matchName(list, name) {
+  return list.filter(function (driverName) {
+    return driverName.name.toLowerCase() === name.toLowerCase();
+  });
+}
