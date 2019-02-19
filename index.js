ec71fdde3d0b17246ff8e02bb46888ec886a4656
Sarah Shin <sarahsujin.shin@gmail.com>
2019-02-18 20:56:55 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Code your solution in this file
+function findMatching(drivers,string){
+  return drivers.filter(function(driversName){
+    return driversName.toUpperCase() === string.toUpperCase();
+  });
+}
+
+function fuzzyMatch(drivers,string){
+  let substring = string.length
+  return drivers.filter(function(driversName){
+    return driversName.slice(0,substring) === string;
+  });
+}
+
+function matchName(drivers,string){
+  return drivers.filter(function(driver){
+    return driver.name.toUpperCase() === string.toUpperCase();
+  });
+}
